5afc2c2cc06f14d4e836ddce4a6ad44cd16f138f
kirill <kirillochnev@gmail.com>
2020-09-23 05:10:09 +0300

    split function def and decl

--- src/mustache/ecs/component_data_storage.hpp
+++ src/mustache/ecs/component_data_storage.hpp
@@ -10,97 +10,41 @@ namespace mustache {
     public:
         explicit ComponentDataStorage(const ComponentMask& mask);
 
-        [[nodiscard]] uint32_t capacity() const noexcept {
-            return chunk_capacity_.toInt() * chunks_.size();
-        }
+        [[nodiscard]] MUSTACHE_INLINE uint32_t capacity() const noexcept;
+        [[nodiscard]] MUSTACHE_INLINE uint32_t size() const noexcept;
+        [[nodiscard]] MUSTACHE_INLINE bool isEmpty() const noexcept;
 
-        [[nodiscard]] uint32_t size() const noexcept {
-            return size_;
-        }
-
-        [[nodiscard]] bool isEmpty() const noexcept {
-            return size_ == 0u;
-        }
-
-        void reserve(size_t new_capacity) {
-            while (capacity() < new_capacity) {
-                allocateChunk();
-            }
-        }
-
-        void incSize() noexcept {
-            ++size_;
-        }
-
-        void decrSize() noexcept {
-            --size_;
-        }
+        MUSTACHE_INLINE void reserve(size_t new_capacity);
+        MUSTACHE_INLINE void incSize() noexcept;
+        MUSTACHE_INLINE void decrSize() noexcept;
 
         template<FunctionSafety _Safety = FunctionSafety::kDefault>
-        void* getEntityData(ComponentStorageIndex index) const noexcept {
-            if constexpr (isSafe(_Safety)) {
-                if (index.isNull() || index.toInt() >= size_) {
-                    return nullptr;
-                }
-            }
-            const auto index_at_chunk = index % chunk_capacity_;
-            const auto read_offset = entityOffset().add(sizeof(Entity) * index_at_chunk.toInt());
-            return chunks_[index / chunk_capacity_]->dataPointerWithOffset(read_offset);
-        }
-        template<FunctionSafety _Safety = FunctionSafety::kDefault>
-        void* getData(ComponentIndex component_index, Chunk* chunk, ChunkEntityIndex index) const noexcept {
-            if constexpr (isSafe(_Safety)) {
-                if (component_index.isNull() || !index.isValid() || chunk == nullptr ||
-                    !component_getter_info_.has(component_index) /*|| index.toInt() >= size_*/ ||
-                    chunk_capacity_.toInt() <= index.toInt()) {
-                    return nullptr;
-                }
-            }
-            const auto& info = component_getter_info_[component_index];
-            const auto offset = info.offset.add(info.size * index.toInt());
-            return chunk->dataPointerWithOffset(offset);
-        }
+        MUSTACHE_INLINE Entity* getEntityData(ComponentStorageIndex index) const noexcept;
 
         template<FunctionSafety _Safety = FunctionSafety::kDefault>
-        void* getData(ComponentIndex component_index, ComponentStorageIndex index) const noexcept {
-            if constexpr (isSafe(_Safety)) {
-                if (component_index.isNull() || index.isNull() ||
-                    !component_getter_info_.has(component_index) || index.toInt() >= size_) {
-                    return nullptr;
-                }
-            }
-            const auto& info = component_getter_info_[component_index];
-            const auto offset = info.offset.add(info.size * index.toInt());
-            auto chunk = chunks_[index / chunk_capacity_];
-            return chunk->dataPointerWithOffset(offset);
-        }
+        MUSTACHE_INLINE void* getData(ComponentIndex component_index, Chunk* chunk, ChunkEntityIndex index) const noexcept;
 
-        void clear(bool free_chunks = true) {
-            if (free_chunks) {
-                for (auto chunk : chunks_) {
-                    freeChunk(chunk);
-                }
-                chunks_.clear();
-            }
-            size_ = 0;
-        }
+        template<FunctionSafety _Safety = FunctionSafety::kDefault>
+        MUSTACHE_INLINE void* getData(ComponentIndex component_index, ComponentStorageIndex index) const noexcept;
 
-        void reserveForNextItem() {
-            reserve(size_ + 1);
-        }
+        MUSTACHE_INLINE void clear(bool free_chunks = true);
 
-        [[nodiscard]] uint32_t componentsCount() const noexcept {
-            return component_getter_info_.size();
-        }
+        MUSTACHE_INLINE void reserveForNextItem();
 
-        [[nodiscard]] ComponentOffset entityOffset() const noexcept {
-            return ComponentOffset::makeAligned(ComponentOffset::make(sizeof(WorldId) * componentsCount()),
-                    alignof(Entity));
-        }
+        [[nodiscard]] MUSTACHE_INLINE uint32_t componentsCount() const noexcept;
 
-        [[nodiscard]] ChunkCapacity chunkCapacity() const noexcept {
-            return chunk_capacity_;
-        }
+        [[nodiscard]] MUSTACHE_INLINE ComponentOffset entityOffset() const noexcept;
+
+        [[nodiscard]] MUSTACHE_INLINE ChunkCapacity chunkCapacity() const noexcept;
+
+        template<FunctionSafety _Safety = FunctionSafety::kDefault>
+        MUSTACHE_INLINE void updateComponentVersion(WorldVersion version, ComponentIndex component_index, ComponentStorageIndex index) noexcept;
+
+        template<FunctionSafety _Safety = FunctionSafety::kDefault>
+        MUSTACHE_INLINE void updateVersion(WorldVersion version, ComponentStorageIndex index) noexcept;
+
+        template<FunctionSafety _Safety = FunctionSafety::kDefault>
+        MUSTACHE_INLINE ComponentStorageIndex pushBackAndUpdateVersion(Entity entity, WorldVersion world_version);
     private:
 
         struct ComponentDataGetter {
@@ -117,4 +61,134 @@ namespace mustache {
         ChunkCapacity chunk_capacity_;
         ArrayWrapper<std::vector<Chunk*>, ChunkIndex> chunks_;
     };
+
+
+    uint32_t ComponentDataStorage::capacity() const noexcept {
+        return chunk_capacity_.toInt() * chunks_.size();
+    }
+
+    uint32_t ComponentDataStorage::size() const noexcept {
+        return size_;
+    }
+
+    bool ComponentDataStorage::isEmpty() const noexcept {
+        return size_ == 0u;
+    }
+
+    void ComponentDataStorage::reserve(size_t new_capacity) {
+        while (capacity() < new_capacity) {
+            allocateChunk();
+        }
+    }
+
+    void ComponentDataStorage::incSize() noexcept {
+        ++size_;
+    }
+
+    void ComponentDataStorage::decrSize() noexcept {
+        --size_;
+    }
+
+    template<FunctionSafety _Safety>
+    Entity* ComponentDataStorage::getEntityData(ComponentStorageIndex index) const noexcept {
+        if constexpr (isSafe(_Safety)) {
+            if (index.isNull() || index.toInt() >= size_) {
+                return nullptr;
+            }
+        }
+        const auto index_at_chunk = index % chunk_capacity_;
+        const auto read_offset = entityOffset().add(sizeof(Entity) * index_at_chunk.toInt());
+        return chunks_[index / chunk_capacity_]->dataPointerWithOffset<Entity>(read_offset);
+    }
+    template<FunctionSafety _Safety>
+    void* ComponentDataStorage::getData(ComponentIndex component_index, Chunk* chunk, ChunkEntityIndex index) const noexcept {
+        if constexpr (isSafe(_Safety)) {
+            if (component_index.isNull() || !index.isValid() || chunk == nullptr ||
+                !component_getter_info_.has(component_index) /*|| index.toInt() >= size_*/ ||
+                chunk_capacity_.toInt() <= index.toInt()) {
+                return nullptr;
+            }
+        }
+        const auto& info = component_getter_info_[component_index];
+        const auto offset = info.offset.add(info.size * index.toInt());
+        return chunk->dataPointerWithOffset(offset);
+    }
+
+    template<FunctionSafety _Safety>
+    void* ComponentDataStorage::getData(ComponentIndex component_index, ComponentStorageIndex index) const noexcept {
+        if constexpr (isSafe(_Safety)) {
+            if (component_index.isNull() || index.isNull() ||
+                !component_getter_info_.has(component_index) || index.toInt() >= size_) {
+                return nullptr;
+            }
+        }
+        const auto& info = component_getter_info_[component_index];
+        const auto offset = info.offset.add(info.size * index.toInt());
+        auto chunk = chunks_[index / chunk_capacity_];
+        return chunk->dataPointerWithOffset(offset);
+    }
+
+    void ComponentDataStorage::clear(bool free_chunks) {
+        if (free_chunks) {
+            for (auto chunk : chunks_) {
+                freeChunk(chunk);
+            }
+            chunks_.clear();
+        }
+        size_ = 0;
+    }
+
+    void ComponentDataStorage::reserveForNextItem() {
+        reserve(size_ + 1);
+    }
+
+    uint32_t ComponentDataStorage::componentsCount() const noexcept {
+        return component_getter_info_.size();
+    }
+
+    ComponentOffset ComponentDataStorage::entityOffset() const noexcept {
+        return ComponentOffset::makeAligned(ComponentOffset::make(sizeof(WorldId) * componentsCount()),
+                                            alignof(Entity));
+    }
+
+    ChunkCapacity ComponentDataStorage::chunkCapacity() const noexcept {
+        return chunk_capacity_;
+    }
+
+    template<FunctionSafety _Safety>
+    void ComponentDataStorage::updateComponentVersion(WorldVersion version, ComponentIndex component_index,
+                                                ComponentStorageIndex index) noexcept {
+        if constexpr (isSafe(_Safety)) {
+            if (!component_getter_info_.has(component_index) || index.toInt() >= size_) {
+                return;
+            }
+        }
+        auto& chunk = chunks_[index / chunk_capacity_];
+        auto version_array = chunk->dataPointerWithOffset<WorldVersion>(ComponentOffset::make(0));
+        version_array[component_index.toInt()] = version;
+    }
+    template<FunctionSafety _Safety>
+    void ComponentDataStorage::updateVersion(WorldVersion version, ComponentStorageIndex index) noexcept {
+        if constexpr (isSafe(_Safety)) {
+            if (index.toInt() >= size_) {
+                return;
+            }
+        }
+        auto& chunk = chunks_[index / chunk_capacity_];
+        auto version_array = chunk->dataPointerWithOffset<WorldVersion>(ComponentOffset::make(0));
+        for (uint32_t i = 0; i < componentsCount(); ++i) {
+            version_array[i] = version;
+        }
+    }
+
+    template<FunctionSafety _Safety>
+    ComponentStorageIndex ComponentDataStorage::pushBackAndUpdateVersion(Entity entity, WorldVersion world_version) {
+        reserveForNextItem();
+        ComponentStorageIndex index = ComponentStorageIndex::make(size_);
+        incSize();
+        *getEntityData<_Safety>(index) = entity;
+        updateVersion(world_version, index);
+        return index;
+    }
+
 }
