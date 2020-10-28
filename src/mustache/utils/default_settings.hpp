4f1191e1e0887df35df2cd28e34b7645c5e4affd
kirill <kirillochnev@gmail.com>
2020-10-28 23:46:52 +0300

    main lib build has been fixed

--- src/mustache/utils/default_settings.hpp
+++ src/mustache/utils/default_settings.hpp
@@ -15,21 +15,35 @@ namespace mustache {
     }
 
 }
-#ifdef _MSC_BUILD
-// NOTE: msvs does not support source_location, so some workaround is needed
-#define MUSTACHE_FILE "unknown"
-#define MUSTACHE_FUNCTION "unknown"
-#define MUSTACHE_LINE 0
+#ifdef __clang__
+    // NOTE: msvs does not support source_location, so some workaround is needed
+    #define MUSTACHE_FILE "unknown"
+    #define MUSTACHE_FUNCTION "unknown"
+    #define MUSTACHE_LINE 0
 
-#define MUSTACHE_INLINE inline
-#define MUSTACHE_RESTRICT_PTR
-#else
-#include <experimental/source_location>
+    #define MUSTACHE_INLINE inline
+    #define MUSTACHE_RESTRICT_PTR
 
-#define MUSTACHE_FILE std::experimental::source_location::current().file_name()
-#define MUSTACHE_FUNCTION std::experimental::source_location::current().function_name()
-#define MUSTACHE_LINE std::experimental::source_location::current().line()
+    #define MUSTACHE_COPY_NOEXCEPT(exp)
+#elif __GNUC__
+    #include <experimental/source_location>
 
-#define MUSTACHE_INLINE inline __attribute__((always_inline))
-#define MUSTACHE_RESTRICT_PTR __restrict
+    #define MUSTACHE_FILE std::experimental::source_location::current().file_name()
+    #define MUSTACHE_FUNCTION std::experimental::source_location::current().function_name()
+    #define MUSTACHE_LINE std::experimental::source_location::current().line()
+
+    #define MUSTACHE_INLINE inline __attribute__((always_inline))
+    #define MUSTACHE_RESTRICT_PTR __restrict
+
+    #define MUSTACHE_COPY_NOEXCEPT(exp) noexcept(noexcept(exp))
+#elif _MSC_VER
+    // NOTE: msvs does not support source_location, so some workaround is needed
+    #define MUSTACHE_FILE "unknown"
+    #define MUSTACHE_FUNCTION "unknown"
+    #define MUSTACHE_LINE 0
+
+    #define MUSTACHE_INLINE inline
+    #define MUSTACHE_RESTRICT_PTR
+
+    #define MUSTACHE_COPY_NOEXCEPT(exp)
 #endif // _MSC_BUILD
