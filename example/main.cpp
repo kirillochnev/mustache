7e6438f8c4d24af9dec95234f735ee21dcb42855
kirillochnev <kirillochnev@gmail.com>
2022-02-06 04:47:13 +0300

    revert example

--- example/main.cpp
+++ example/main.cpp
@@ -1,38 +1,32 @@
-//#include <mustache/ecs/entity.hpp>
-//#include <mustache/ecs/job.hpp>
-
-#include <iostream>
-//#include <mustache/ecs/entity_manager.hpp>
-//#include <mustache/utils/context.hpp>
-#include <mustache/ecs/component_factory.hpp>
-#include <mustache/ecs/entity_manager.hpp>
-#include <mustache/ecs/world.hpp>
-#include <mustache/ecs/non_template_job.hpp>
-//#include <mustache/utils/profiler.hpp>
-//#include <mustache/ecs/world.hpp>
-#include <mustache/utils/benchmark.hpp>
-
-#include <glm/gtc/quaternion.hpp>
-#include <glm/vec3.hpp>
-#include <mustache/utils/dispatch.hpp>
-#include <mustache/ecs/job.hpp>
-#include <mustache/utils/logger.hpp>
+#include <mustache/ecs/ecs.hpp>
 
 namespace {
+    struct Vec3 {
+        float x {0.0f};
+        float y {0.0f};
+        float z {0.0f};
+    };
+    struct Quaternion {
+        float x {0.0f};
+        float y {0.0f};
+        float z {0.0f};
+        float w {1.0f};
+    };
+
     struct Position {
-        glm::vec3 value;
+        Vec3 value;
     };
 
     struct Velocity {
-        float value{1};
+        float value {1};
     };
 
     struct Rotation {
-        glm::quat orient;
+        Quaternion value;
     };
 
-    MUSTACHE_INLINE glm::vec3 forward(const glm::quat& q) {
-        return glm::vec3{
+    MUSTACHE_INLINE Vec3 forward(const Quaternion& q) {
+        return Vec3 {
                 -2.0f * (q.x * q.z + q.w * q.y),
                 -2.0f * (q.y * q.z - q.w * q.x),
                 -1.0f + 2.0f * (q.x * q.x + q.y * q.y),
@@ -40,84 +34,53 @@ namespace {
     }
 }
 
-void iterate500k() {
-    enum : uint32_t {
-        kNumObjects = 500000,
-        kNumIteration = 100,
-    };
-    mustache::World world{mustache::WorldId::make(0)};
+void createEntities(mustache::World& world, uint32_t entities_count, uint32_t mode) {
     auto& entities = world.entities();
-    auto& archetype = entities.getArchetype<Position, Velocity, Rotation>();
-    {
-        for (uint32_t i = 0; i < kNumObjects; ++i) {
-            (void) entities.create(archetype);
+    auto& archetype = world.entities().getArchetype<Position, Velocity, Rotation>();
+    for (uint32_t i = 0; i < entities_count; ++i) {
+        switch (mode) {
+            case 0:
+                (void)world.entities().create(archetype); // fastest way to create
+                break;
+            case 1:
+                (void)world.entities().create<Position, Velocity, Rotation>();
+                break;
+            case 2:
+                // you can initialize components using next form
+                (void)world.entities().begin()
+                    .assign<Position>(1.0f, 2.0f, 3.0f) // {} initializer used
+                    .assign<Velocity>(Velocity{1.0f}) // copy/move constructor
+                    .assign<Rotation>() // default constructor (or nothing for trivially default constructible components)
+                .end();
+                break;
+            case 3:
+            {   // For better performance you should avoid such way to create entity
+                const auto entity = world.entities().create();
+                world.entities().assign<Position>(entity, 1.0f, 2.0f, 3.0f); // {} initializer
+                world.entities().assign<Velocity>(entity);
+                world.entities().assign<Rotation>(entity);
+            }
+                break;
         }
     }
-    constexpr float dt = 1.0f / static_cast<float>(kNumIteration);
-
-    mustache::Benchmark benchmark;
-
-    mustache::Logger{}.info("Task Begin!");
-
-    for (uint32_t i = 0; i < kNumIteration; ++i) {
-
-        benchmark.add([&entities] {
-            entities.forEach([](Position& pos, const Velocity& vel, const Rotation& rot) {
-                pos.value += dt * vel.value * forward(rot.orient);
-            }, mustache::JobRunMode::kParallel);
-        });
-    }
-    benchmark.show();
-
 }
-void non_template_iterate500k() {
-    enum : uint32_t {
-        kNumObjects = 500000,
-        kNumIteration = 100,
-    };
-    mustache::World world{mustache::WorldId::make(0)};
-    auto& entities = world.entities();
-    auto& archetype = entities.getArchetype<Position, Velocity, Rotation>();
-    {
-        for (uint32_t i = 0; i < kNumObjects; ++i) {
-            (void) entities.create(archetype);
-        }
-    }
-    constexpr float dt = 1.0f / static_cast<float>(kNumIteration);
-    mustache::NonTemplateJob job;
-
-    job.component_requests = {
-            {mustache::ComponentFactory::registerComponent<Position>(), false, true},
-            {mustache::ComponentFactory::registerComponent<Velocity>(), true, true},
-            {mustache::ComponentFactory::registerComponent<Rotation>(), true, true}
-    };
-
-    job.callback = [](const mustache::NonTemplateJob::ForEachArrayArgs& args) {
-        Position* MUSTACHE_RESTRICT_PTR pos = static_cast<Position*>(args.components[0]);
-        const Velocity* MUSTACHE_RESTRICT_PTR vel = static_cast<const Velocity*>(args.components[1]);
-        const Rotation* MUSTACHE_RESTRICT_PTR rot = static_cast<const Rotation*>(args.components[2]);
-        for (uint32_t i = 0; i < args.count.toInt(); ++i) {
-            pos[i].value += dt * vel[i].value * forward(rot[i].orient);
-        }
-    };
-
-    mustache::Benchmark benchmark;
-
-    mustache::Logger{}.info("Task Begin!");
-
-    for (uint32_t i = 0; i < kNumIteration; ++i) {
-        benchmark.add([&world, &job] {
-            job.run(world, mustache::JobRunMode::kParallel);
-        });
-    }
-    benchmark.show();
-
-}
-
 
 int main() {
+    constexpr uint32_t entities_count = 10;
+    constexpr float dt = 1.0f / 60.0f;
 
-//    iterate500k();
-    non_template_iterate500k();
+    mustache::World world;
+    createEntities(world, 10, 3);
+
+    const auto run_mode = mustache::JobRunMode::kCurrentThread; // or kParallel
+
+    world.entities().forEach([](Position& pos, const Velocity& vel, const Rotation& rot) {
+        const float dist = dt * vel.value;
+        const auto dir = forward(rot.value);
+        pos.value.x += dist * dir.x;
+        pos.value.y += dist * dir.y;
+        pos.value.z += dist * dir.z;
+    }, run_mode);
+
     return 0;
 }
