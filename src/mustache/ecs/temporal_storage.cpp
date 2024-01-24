2324be54ee05329413a64414c880b5aff6bcd544
kirill.ochnev <kirillochnev@gmail.com>
2024-01-24 08:23:40 +0300

    profiler marks added into temporal storage

--- src/mustache/ecs/temporal_storage.cpp
+++ src/mustache/ecs/temporal_storage.cpp
@@ -1,8 +1,10 @@
 #include "temporal_storage.hpp"
+#include <mustache/utils/profiler.hpp>
 
 using namespace mustache;
 
 TemporalStorage::~TemporalStorage() {
+    MUSTACHE_PROFILER_BLOCK_LVL_0(__FUNCTION__);
     for (const auto& action : actions_) {
         if (action.action == Action::kCreateEntity &&
             action.type_info != nullptr && action.ptr != nullptr
@@ -14,6 +16,7 @@ TemporalStorage::~TemporalStorage() {
 }
 
 void* TemporalStorage::assignComponent(World& world, Entity entity, ComponentId id, bool skip_constructor) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     const auto& component_info = ComponentFactory::instance().componentInfo(id);
     auto& command = emplaceItem(entity, Action::kAssignComponent);
     command.type_info = &component_info;
@@ -26,6 +29,7 @@ void* TemporalStorage::assignComponent(World& world, Entity entity, ComponentId 
 }
 
 void TemporalStorage::create(Entity entity, const ComponentIdMask& mask, const SharedComponentsInfo& shared) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     auto& command = emplaceItem(entity, Action::kCreateEntity);
     //command.archetype = archetype;
     if (!mask.isEmpty() || !shared.empty()) {
@@ -37,19 +41,23 @@ void TemporalStorage::create(Entity entity, const ComponentIdMask& mask, const S
 }
 
 void TemporalStorage::removeComponent(Entity entity, ComponentId id) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     auto& command = emplaceItem(entity, Action::kRemoveComponent);
     command.component_id = id;
 }
 
 void TemporalStorage::destroy(Entity entity) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     emplaceItem(entity, Action::kDestroyEntity);
 }
 
 void TemporalStorage::destroyNow(Entity entity) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     emplaceItem(entity, Action::kDestroyEntityNow);
 }
 
 void TemporalStorage::clear() {
+    MUSTACHE_PROFILER_BLOCK_LVL_0(__FUNCTION__);
     actions_.clear();
     create_actions_.clear();
     if (chunks_.size() > 1u) {
@@ -64,6 +72,7 @@ void TemporalStorage::clear() {
 }
 
 std::byte* TemporalStorage::allocate(uint32_t size) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     if (chunks_.empty() || chunks_.back().free_space < size) {
         if (target_chunk_size_ < size) {
             target_chunk_size_ = size;
@@ -78,6 +87,7 @@ std::byte* TemporalStorage::allocate(uint32_t size) {
 }
 
 TemporalStorage::ActionInfo& TemporalStorage::emplaceItem(Entity entity, Action action) {
+    MUSTACHE_PROFILER_BLOCK_LVL_3(__FUNCTION__);
     return actions_.emplace_back(entity, action);
 }
 
